1c5a5249efc1f387dce5071c1271fe1745aee4ae
Gavin Borne <zentiphdev@gmail.com>
2025-04-07 23:05:07 -0400

    initialize react

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <a href="#">
+          <img src="/code-quilt-logo.svg" alt="CodeQuilt" />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
