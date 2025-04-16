354680cafbd537067dc4930eb7760bed85f49416
Gavin Borne <zentiphdev@gmail.com>
2025-04-16 00:18:53 -0400

    implement navbar resizing and start hamburger menu (NEEDS WORK)

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -16,6 +16,8 @@
  * @author @Zentiph
  */
 
+import { useState } from "react";
+
 import Link from "./Link";
 
 import "./Navbar.css";
@@ -24,10 +26,16 @@ import "./Navbar.css";
  * @returns Navbar header for the pages other than projects
  */
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav>
-      <ul id="site-nav">
-        <li id="logo">
+      <div id="left-nav">
+        <div id="logo">
           <Link href="/">
             <img
               className="transition grow-on-hover"
@@ -35,45 +43,53 @@ const Navbar = () => {
               alt="CodeQuilt"
             />
           </Link>
-        </li>
-        {/* TODO: UPDATE THESE LINKS WHEN AVAILABLE */}
-        <li>
-          {/* Browse all games rather than the select few displayed on the homepage */}
-          <Link href="#">
-            <span className="transition grow-on-hover">Browse</span>
-          </Link>
-        </li>
-        <li>
-          {/* About this project and the devs */}
-          <Link href="#">
-            <span className="transition grow-on-hover">Meet the Team</span>
-          </Link>
-        </li>
-      </ul>
+        </div>
+        <ul className={menuOpen ? "show" : ""}>
+          {/* TODO: UPDATE THESE LINKS WHEN AVAILABLE */}
+          <li>
+            {/* Browse all games rather than the select few displayed on the homepage */}
+            <Link href="#">
+              <span className="transition grow-on-hover">Browse</span>
+            </Link>
+          </li>
+          <li>
+            {/* About this project and the devs */}
+            <Link href="#">
+              <span className="transition grow-on-hover">Meet the Team</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-      <ul id="external-nav">
-        <li>
-          <Link href="https://ko-fi.com/zentiph" className="img-link">
-            <img
-              src="/ko-fi.svg"
-              alt="CodeQuilt Ko-Fi for Donations"
-              className="transition grow-on-hover"
-            />
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://github.com/justianisdev/CodeQuilt"
-            className="img-link"
-          >
-            <img
-              src="/github.svg"
-              alt="CodeQuilt GitHub"
-              className="transition grow-on-hover"
-            />
-          </Link>
-        </li>
-      </ul>
+      <div id="right-nav">
+        <ul>
+          <li>
+            <Link
+              href="https://github.com/justianisdev/CodeQuilt"
+              className="img-link"
+            >
+              <img
+                src="/github.svg"
+                alt="CodeQuilt GitHub"
+                className="transition grow-on-hover"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://ko-fi.com/zentiph" className="img-link">
+              <img
+                src="/ko-fi.svg"
+                alt="CodeQuilt Ko-Fi for Donations"
+                className="transition grow-on-hover"
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <button id="hamburger" onClick={toggleMenu}>
+        <img src="/burger-menu.svg" alt="Menu" />
+      </button>
     </nav>
   );
 };
